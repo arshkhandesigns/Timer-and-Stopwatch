bf51cce4e7f08aea14e8c74914362c3f1447812d
ARSH KHAN <arsh.khan.cs.2020@mitmeerut.ac.in>
2023-08-14 17:04:20 +0530

    sat

--- script.js
+++ script.js
@@ -1,36 +1,67 @@
+const timerDisplay = document.getElementById('timerDisplay');
+const startTimerBtn = document.getElementById('startTimer');
+const pauseTimerBtn = document.getElementById('pauseTimer');
+const resetTimerBtn = document.getElementById('resetTimer');
+
+const stopwatchDisplay = document.getElementById('stopwatchDisplay');
+const startStopwatchBtn = document.getElementById('startStopwatch');
+const pauseStopwatchBtn = document.getElementById('pauseStopwatch');
+const resetStopwatchBtn = document.getElementById('resetStopwatch');
+
 let timerInterval;
-let startTime = 0;
-let isRunning = false;
+let stopwatchInterval;
 
-function formatTime(ms) {
-  const date = new Date(ms);
-  return date.toISOString().substr(11, 8);
+function updateDisplay(display, time) {
+  display.textContent = time;
 }
 
-function updateTimer() {
-  const currentTime = new Date().getTime();
-  const elapsedTime = currentTime - startTime;
-  document.getElementById('timer').textContent = formatTime(elapsedTime);
+function startTimer() {
+  let seconds = 0;
+  timerInterval = setInterval(() => {
+    seconds++;
+    const formattedTime = formatTime(seconds);
+    updateDisplay(timerDisplay, formattedTime);
+  }, 1000);
 }
 
-document.getElementById('startTimer').addEventListener('click', () => {
-  if (!isRunning) {
-    isRunning = true;
-    startTime = new Date().getTime();
-    timerInterval = setInterval(updateTimer, 1000);
-  }
-});
-
-document.getElementById('stopTimer').addEventListener('click', () => {
-  if (isRunning) {
-    clearInterval(timerInterval);
-    isRunning = false;
-  }
-});
-
-document.getElementById('resetTimer').addEventListener('click', () => {
+function pauseTimer() {
   clearInterval(timerInterval);
-  isRunning = false;
-  startTime = 0;
-  document.getElementById('timer').textContent = formatTime(0);
-});
+}
+
+function resetTimer() {
+  clearInterval(timerInterval);
+  updateDisplay(timerDisplay, '00:00:00');
+}
+
+function startStopwatch() {
+  let seconds = 0;
+  stopwatchInterval = setInterval(() => {
+    seconds++;
+    const formattedTime = formatTime(seconds);
+    updateDisplay(stopwatchDisplay, formattedTime);
+  }, 1000);
+}
+
+function pauseStopwatch() {
+  clearInterval(stopwatchInterval);
+}
+
+function resetStopwatch() {
+  clearInterval(stopwatchInterval);
+  updateDisplay(stopwatchDisplay, '00:00:00');
+}
+
+function formatTime(totalSeconds) {
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+startTimerBtn.addEventListener('click', startTimer);
+pauseTimerBtn.addEventListener('click', pauseTimer);
+resetTimerBtn.addEventListener('click', resetTimer);
+
+startStopwatchBtn.addEventListener('click', startStopwatch);
+pauseStopwatchBtn.addEventListener('click', pauseStopwatch);
+resetStopwatchBtn.addEventListener('click', resetStopwatch);
